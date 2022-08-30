423327e793d783231d7c3fce94be08bfb965e810
Ezequiel <55319859+zico15@users.noreply.github.com>
2022-08-30 17:15:32 +0100

    Add files via upload

---
+++ demo1/src/main/java/com/example/demo1/HelloController.java
@@ -0,0 +1,12 @@
+package com.example.demo1;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+
+
+    @FXML
+    protected void onHelloButtonClick(){
+    }
+}
---
+++ demo1/src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module com.example.demo1 {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires com.almasb.fxgl.all;
+
+    opens com.example.demo1 to javafx.fxml;
+    exports com.example.demo1;
+}
