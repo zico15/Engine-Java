7bd88f66cd413d22c1b9f5d3006a75430f7d992e
edos-san <zequielzico@gmail.com>
2022-09-06 06:51:31 +0100

    add style.css

--- demo1/src/main/java/com/example/demo1/HelloController.java
+++ demo1/src/main/java/com/example/demo1/HelloController.java
@@ -1,20 +1,37 @@
 package com.example.demo1;
 
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Control;
 import javafx.scene.control.Label;
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 public class HelloController extends Control {
 
     @FXML
     private TreeView<String> treeView;
-
+    TreeItem rootItem = new TreeItem("Tutorials");
+    private final Node rootIcon = new ImageView( );
 
     @FXML
+    private void initialize()
+    {
+
+        System.out.println("initialize");
+        TreeItem webItem = new TreeItem("Web Tutorials", rootIcon);
+        webItem.getChildren().add(new TreeItem("HTML  Tutorial", rootIcon));
+        webItem.getChildren().add(new TreeItem("HTML5 Tutorial", rootIcon));
+        webItem.getChildren().add(new TreeItem("CSS Tutorial", rootIcon));
+        webItem.getChildren().add(new TreeItem("SVG Tutorial", rootIcon));
+        rootItem.getChildren().add(webItem);
+        treeView.setRoot(rootItem);
+    }
+    @FXML
     protected void onHelloButtonClick(){
-        System.out.println("sdsad");
+
         TreeItem t = new TreeItem<>();
         t.setValue("sasd");
         treeView.getSelectionModel().getSelectedItems().add(t);
