ee28cc958f28cc2c1979d55802584189505c1baf
edos-san <zequielzico@gmail.com>
2023-01-20 03:19:37 +0000

    getPackage

--- Game Plugin/src/game/components/properties/ScriptProperties.java
+++ Game Plugin/src/game/components/properties/ScriptProperties.java
@@ -1,9 +1,13 @@
 package game.components.properties;
 
+import com.properties.components.Layouts;
+import com.view.DialogPane;
 import game.components.view.objects.GameObjectProperties;
+import game.components.view.resources.ResourceTreeView;
 import game.core.components.Script;
 import game.project.GameEngine;
 import javafx.scene.control.MenuItem;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.control.TextField;
 
@@ -14,6 +18,8 @@ public class ScriptProperties extends ComponentProperties<VBox> {
     private String file;
 
     private Script script = new Script();
+
+    private static ResourceTreeView resourceTreeView = new ResourceTreeView();
     private TextField textField;
     public ScriptProperties() {
         super( new VBox());
@@ -33,9 +39,15 @@ public class ScriptProperties extends ComponentProperties<VBox> {
     public void init() {
         textField = new TextField();
         textField.setOnAction(e -> {
-            file = textField.getText().trim();
+            DialogPane  dialogPane = new DialogPane("Scripts");
+            resourceTreeView.load(GameEngine.gameProject.getDirectory());
+            AnchorPane pane = new AnchorPane(resourceTreeView);
+            Layouts.alignment(resourceTreeView, Layouts.ALL);
+            dialogPane.setPane(pane);
+            dialogPane.showAndWait(null);
+            /*file = textField.getText().trim();
             script.setFile(new File(GameEngine.gameProject.getDirectory(), file));
-            System.out.printf("file: " + file);
+            System.out.printf("file: " + file);*/
         });
         ComponentLayouts.addItem(ComponentLayouts.addTitle("Script"), getView());
         ComponentLayouts.addItem(textField, getView());
